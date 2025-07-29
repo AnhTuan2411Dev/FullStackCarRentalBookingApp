4960fe3dadbb53fc12f23ddc4d8a066cf658eb24
AnhTuan2411Dev <anhtuan2411.dev@gmail.com>
2025-07-29 21:21:07 +0700

    Tạo Navigation Bar, Tạo các trang chính

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,23 @@
 import './App.css'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Cars from './pages/Cars'
+import MyBookings from './pages/MyBookings'
+import CarDetails from './pages/CarDetails'
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </>
+    <div className='bg-light'>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cars" element={<Cars />} />
+        <Route path="/my-bookings" element={<MyBookings />} />
+        <Route path="/cars/:id" element={<CarDetails />} />
+      </Routes>
+    </div>
   )
 }
 
